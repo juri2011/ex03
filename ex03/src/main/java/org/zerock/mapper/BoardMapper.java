93e80379c54b9551f9ec52aa1f122d105fb91b86
juri <ojl98@naver.com>
2024-03-11 12:48:07 +0900

    BoardMapper interface에insert, insertSelectKey작성

--- ex03/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex03/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -8,4 +8,10 @@ public interface BoardMapper {
   
   //@Select("select * from tbl_board where bno > 0")
   public List<BoardVO> getList();
+  
+  //데이터 삽입만
+  public void insert(BoardVO board);
+  
+  //데이터 삽입하고 key까지 가져옴
+  public void insertSelectKey(BoardVO board);
 }
