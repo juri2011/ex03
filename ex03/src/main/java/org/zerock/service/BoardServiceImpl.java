7bc6ad1e69c0d4c4dcd006bcdc076c29c4c85701
juri <ojl98@naver.com>
2024-03-11 15:53:50 +0900

    BoardServiceImpl에 클래스레벨 annotation 추가 후 setter에 대한 주석 작성

--- ex03/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ ex03/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -2,10 +2,30 @@ package org.zerock.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
 
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+@AllArgsConstructor
 public class BoardServiceImpl implements BoardService {
-
+  
+  //@Setter는 해당 필드의 setter를 생성한다. 클래스 레벨은 물론이고 필드레벨에도 사용 가능하다.
+  //@Setter(onMethod_ = @Autowired) : setter가 생성되면 @Autowired를 붙인다.
+  /*
+     @Autowired : BoardServiceImpl을 생성할 때 스프링의 AutowiredAnnotationBeanPostProcessor가
+     BoardMapper 타입의 빈을 얻어서 mapper 필드에 자동으로 연결한다.
+     (지금은 Spring이 controller 패키지 안의 컴포넌트만 스캔하도록 설정 되어있는데, 과연 이 상태에서 의존주입이 될까?)
+  */
+  @Setter(onMethod_ = @Autowired)
+  private BoardMapper mapper;
+  
   @Override
   public void register(BoardVO board) {
     
