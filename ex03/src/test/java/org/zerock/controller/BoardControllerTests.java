d4adefa6b81cd47757e8f3a4ce57826218c47d5a
juri <ojl98@naver.com>
2024-03-12 13:04:32 +0900

    BoardControllerTests에 testRegister(): 게시글 등록 테스트 완료

--- ex03/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex03/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.mvc.method.annotation.ModelAndViewMethodReturnValueHandler;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -60,6 +61,20 @@ public class BoardControllerTests {
         .andReturn()
         .getModelAndView()
         .getModelMap());
+  }
+  
+  @Test
+  public void testRegister() throws Exception {
+    
+    String resultPage
+    = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+        .param("title","테스트 새글 제목")
+        .param("content","테스트 새글 내용")
+        .param("writer","user00")
+        ).andReturn().getModelAndView().getViewName();
+    
+    log.info(resultPage);
+    
   }
 
 }
