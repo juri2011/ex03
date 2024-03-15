2b83a5c8dcf41b5297fcff296831874ad5c3c3e7
juri <ojl98@naver.com>
2024-03-15 14:55:23 +0900

    Criteria : 필드, 생성자 생성

--- ex03/src/main/java/org/zerock/domain/Criteria.java
+++ ex03/src/main/java/org/zerock/domain/Criteria.java
@@ -9,4 +9,16 @@ import lombok.ToString;
 @ToString
 public class Criteria {
   
+  private int pageNum;
+  private int amount;
+  
+  //파라미터가 없을 경우 1페이지 출력
+  public Criteria() {
+    this(1, 10);
+  }
+  //생성자 오버로딩
+  public Criteria(int pageNum, int amount) {
+    this.pageNum = pageNum;
+    this.amount = amount;
+  }
 }
